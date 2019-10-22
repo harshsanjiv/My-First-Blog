fd0511469f83bded0fe26e6be90f2ce39ee336a2
HARSH <harshsanjiv@gmail.com>
2019-10-22 11:59:40 +0530

    way too many changes

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,13 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from blog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
+     path('post/new/', views.post_new, name='post_new'),
+     path('post/<int:pk>/edit/',views.post_edit,name="post_edit")
 ]
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 
 urlpatterns += staticfiles_urlpatterns()
+
